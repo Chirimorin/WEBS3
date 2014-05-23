9eb2d2517424556fe402c2fb60c9dfe4d0c80ae7
Thomas Voogt <chirimorin@gmail.com>
2014-05-23 15:54:43 +0200

    begin navigation

--- app/views/shared/head.blade.php
+++ app/views/shared/head.blade.php
@@ -6,4 +6,5 @@
 
 {{ HTML::script('//code.jquery.com/jquery-1.10.2.min.js') }}
 {{ HTML::script('assets/js/bootstrap.js') }}
+{{ HTML::script('assets/js/scripts.js') }}
 <meta name="author" content="Michael van de Ven & Thomas Voogt" />
