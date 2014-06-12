97f44c84fc86ea0dc9c7f12619e5dcd0f31a9384
Thomas Voogt <chirimorin@gmail.com>
2014-06-12 16:30:48 +0200

    forum parent nullable, default topic views 0

--- app/database/migrations/2014_05_16_130351_create_topic_table.php
+++ app/database/migrations/2014_05_16_130351_create_topic_table.php
@@ -17,7 +17,7 @@ class CreateTopicTable extends Migration {
 			$table->increments('id');
             $table->integer('forum_id')->unsigned();
             $table->foreign('forum_id')->references('id')->on('forum');
-            $table->integer('views');
+            $table->integer('views')->default(0);
             $table->timestamp('done_at')->nullable();
 			$table->timestamps();
             $table->boolean('public');
